2de3f561c12c0e6da1c1dd320d0253ba532fef6f
@Sinha199 <anshu.resource@gmail.com>
2021-07-18 22:48:36 +0530

    added controller for deleting and updating

--- server/controllers/book-controller.js
+++ server/controllers/book-controller.js
@@ -32,7 +32,59 @@ createBook = (req,res) =>{
         })
 }
 
+updateBook = async (req,res) =>{
 
-getBook = (req,res) =>{
+    const body = req.body
+    if(!body){
+        return res.status(400).json({
+            success:false,
+            error:'Something missing , must provide body to update',
+        })
+    }
 
+    Book.findOne({_id:req.params.id},(err,book)=>{
+        if(err){
+            return res.status(404).json({
+                err,
+                message:'Oops:( book not found',
+            })
+        }
+
+        book.name=body.name
+        book.pages=body.pages
+        book.genre=body.genre
+
+        book
+            .save()
+            .then(()=>{
+                return res.status(200).json({
+                    success:true,
+                    id:book._id,
+                    meesage:'Book updated!',
+                })
+            })
+            .catch(error=>{
+                return res.status(404).json({
+                    error,
+                    message:'Oops , Book not updated!',
+                })
+            })
+    })
+}
+
+deleteBook = async(req,res)=>{
+    await Book.findOneAndDelete({_id:req.params.id},(err,movie)=>{
+        if(err){
+            return res.status(400).json({success:false,error:err})
+        }
+        if(!book)
+        {
+            return res
+                .status(404)
+                .json({success:false,error:'Book not found'})
+        }
+        return res.status(200).json({success:true,data:book})
+
+    })
+    .catch(err=>console.log(err))
 }
