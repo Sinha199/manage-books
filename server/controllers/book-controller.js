52e586d51673ad8b1afa5a273927ac2255b88df3
@Sinha199 <anshu.resource@gmail.com>
2021-07-18 20:14:08 +0530

    added controller for creating books

--- server/controllers/book-controller.js
+++ server/controllers/book-controller.js
@@ -9,5 +9,30 @@ createBook = (req,res) =>{
             error:'You must provide a book',
         })
     }
+    const book = new Book(body)
+    
+    if(!book){
+        return res.status(400).json({success:false,error:err})
+    }
+
+    book
+        .save()
+        .then(()=> {
+            return res.status(201).json({
+                success:true,
+                id:book._id,
+                message:'Book Added!',
+            })
+        })
+        .catch(error =>{
+            return res.status(400).json({
+                error,
+                message:'Oops! Something went wrong!',
+            })
+        })
 }
 
+
+getBook = (req,res) =>{
+
+}
