4643ec1ac8a97454fe37dfbde408489eafcdea21
@Sinha199 <anshu.resource@gmail.com>
2021-09-18 18:11:17 +0530

    added page/bookupdate

--- client/src/pages/BookList.jsx
+++ client/src/pages/BookList.jsx
@@ -1,11 +1,112 @@
 import React, { Component } from 'react'
+import ReactTable from 'react-table'
+
+import api from '../api'
+import styled from 'styled-components'
+import 'react-table/react-table.css'
+
+const Wrapper = styled.div`
+    padding: 0 40px 40px 40px;
+`
+
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor:pointer;
+
+`
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
+class UpdateBook extends Component {
+    updateUser = event => {
+        event.preventDefault()
+
+        window.location.href = `/books/update/${this.props.id}`
+    }
+
+    render() {
+        return <Update onClick={this.updateUser}>Update</Update>
+    }
+}
+
+class DeleteBook extends Component {
+    deleteUser = event => {
+        event.preventDefault()
+
+        if (
+            window.confirm(
+                `Do tou want to delete the book ${this.props.id} permanently?`,
+            )
+        ) {
+            api.deleteBook(this.props.id)
+            window.location.reload()
+        }
+    }
+
+    render() {
+        return <Delete onClick={this.deleteUser}>Delete</Delete>
+    }
+}
 
 class BookList extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            movies:[],
+            columns:[],
+            isLoading:false,
+        }
+    }
+
+    componentDidMount=async () =>{
+        this.setState({isLoading:true})
+        await api.getBook().then(book =>{
+            this.setState({
+                books:books.data.data,
+                isLoading:false,
+            })
+        })
+    }
     render() {
+        const {books,isLoading}=this.state
+        console.log('TCL:BookList-> render-> books',books)
+        
+        const columns = [
+            {
+                Header: 'Name',
+                accessor: 'name',
+                filterable: true,
+            },
+            {
+                Header: 'Pages',
+                accessor: 'pages',
+                filterable: true,
+            },
+            {
+                Header: 'Genre',
+                accessor: 'genre',
+                filterable: true,
+            },
+        ]
+
+        let showTable = true
+        if(!books.length){
+            showTable=false
+        }
         return (
-            <div>
-                <p>In this page you'll see the list of movies</p>
-            </div>
+            <Wrapper>
+                {showTable && (
+                    <ReactTable
+                        data={movies}
+                        columns={columns}
+                        loading={isLoading}
+                        defaultPageSize={10}
+                        showPageSizeOptions={true}
+                        minRows={0}
+                    />
+                )}
+            </Wrapper>
         )
     }
 }
--- client/src/pages/BookUpdate.jsx
+++ client/src/pages/BookUpdate.jsx
@@ -1,11 +1,130 @@
 import React, { Component } from 'react'
+import api from '../api'
+
+import styled from 'styled-components'
+
+const Title = styled.h1.attrs({
+    className: 'h1',
+})``
+
+const Wrapper = styled.div.attrs({
+    className: 'form-group',
+})`
+    margin: 0 30px;
+`
+
+const Label = styled.label`
+    margin: 5px;
+`
+
+const InputText = styled.input.attrs({
+    className: 'form-control',
+})`
+    margin: 5px;
+`
+
+const Button = styled.button.attrs({
+    className: `btn btn-primary`,
+})`
+    margin: 15px 15px 15px 5px;
+`
+
+const CancelButton = styled.a.attrs({
+    className: `btn btn-danger`,
+})`
+    margin: 15px 15px 15px 5px;
+`
 
 class BookUpdate extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            name: '',
+            rating: '',
+            time: '',
+        }
+    }
+
+    handleChangeInputName = async event => {
+        const name = event.target.value
+        this.setState({ name })
+    }
+
+    handleChangeInputRating = async event => {
+        const rating = event.target.validity.valid
+            ? event.target.value
+            : this.state.rating
+
+        this.setState({ rating })
+    }
+
+    handleChangeInputGenre = async event => {
+        const name = event.target.value
+        this.setState({ genre })
+    }
+
+    handleUpdateBook = async () => {
+        const { id, name, genre } = this.state
+        const payload = { name, rating, genre}
+
+        await api.updateMovieById(id, payload).then(res => {
+            window.alert(`Movie updated successfully`)
+            this.setState({
+                name: '',
+                rating: '',
+                genre: '',
+            })
+        })
+    }
+
+    componentDidMount = async () => {
+        const { id } = this.state
+        const movie = await api.getMovieById(id)
+
+        this.setState({
+            name: movie.data.data.name,
+            rating: movie.data.data.rating,
+            time: movie.data.data.time.join('/'),
+        })
+    }
+
     render() {
+        const { name, rating, time } = this.state
         return (
-            <div>
-                <p>In this page you'll see the form to update the movies</p>
-            </div>
+            <Wrapper>
+                <Title>Create Book</Title>
+
+                <Label>Name: </Label>
+                <InputText
+                    type="text"
+                    value={name}
+                    onChange={this.handleChangeInputName}
+                />
+
+                <Label>Rating: </Label>
+                <InputText
+                    type="number"
+                    step="0.1"
+                    lang="en-US"
+                    min="0"
+                    max="10"
+                    pattern="[0-9]+([,\.][0-9]+)?"
+                    value={rating}
+                    onChange={this.handleChangeInputRating}
+                />
+
+                <Label>Genre: </Label>
+                <InputText
+                    type="text"
+                    value={genre}
+                    onChange={this.handleChangeInputGenre}
+                />
+
+                <Button onClick={this.handleUpdateBook}>Update Book</Button>
+                <CancelButton href={'/movies/list'}>Cancel</CancelButton>
+            </Wrapper>
         )
     }
 }
